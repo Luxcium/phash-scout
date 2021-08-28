218afae718132958138e2dfc6fb33b2c9360afdb
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-08-28 09:37:34 -0400

    REFACTOR: converting into a typescript project
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

---
+++ index.ts
@@ -0,0 +1,96 @@
+/*------------------------------------------------------------------------------
+ *  Based on work from @userpixel (https://github.com/userpixel)
+ *  Copyright (c) 2020 Alex Ewerlöf
+ *  Licensed under the MIT License.
+ *  See https://github.com/userpixel/cap-parallel/blob/master/LICENSE.md
+ *  for original license information.
+ *----------------------------------------------------------------------------*/
+
+type Mapper = <T, U>(value: T, index: number, array: readonly T[]) => U;
+
+import { promisify } from 'util';
+
+const setImmediateP = promisify(setImmediate);
+
+async function mapItem<T>(
+  mapFn: Mapper,
+  currentValue: T,
+  index: number,
+  array: T[]
+) {
+  try {
+    await setImmediateP();
+    return {
+      status: 'fulfilled',
+      value: await mapFn(currentValue, index, array),
+    };
+  } catch (reason) {
+    return {
+      status: 'rejected',
+      reason,
+    };
+  }
+}
+
+async function worker<T>(
+  gen: Generator<[T, number, T[]]>,
+  mapFn: Mapper,
+  result: any
+) {
+  for (let [currentValue, index, array] of gen) {
+    result[index] = await mapItem(mapFn, currentValue, index, array);
+  }
+}
+
+function* arrayGenerator<T>(array: T[]): Generator<[T, number, T[]]> {
+  for (let index = 0; index < array.length; index++) {
+    const currentValue = array[index];
+    yield [currentValue, index, array];
+  }
+}
+
+async function mapAllSettled<T, U>(
+  arr: T[],
+  mapFn: Mapper,
+  limit: number = arr.length
+): Promise<U[]> {
+  const result: U[] = [];
+
+  if (arr.length === 0) {
+    return result;
+  }
+
+  const gen /* :[T,number,T[]] */ = arrayGenerator(arr);
+
+  limit = Math.min(limit, arr.length);
+
+  const workers = new Array(limit);
+  for (let i = 0; i < limit; i++) {
+    workers.push(worker(gen, mapFn, result));
+  }
+
+  // console.log(`Initialized ${limit} workers`)
+
+  await Promise.all(workers);
+
+  return result;
+}
+
+// const urls = []
+// for (let i = 0; i < 100; i++) {
+//     urls.push(`https://www.google.com/search?q=${i}`)
+// }
+
+// async function mapFn(url, i) {
+//     const contents = await got(url)
+//     return { i, url, contents }
+// }
+
+async function capParallel<T, U>(
+  arr: T[],
+  mapFn: Mapper,
+  limit: number = arr.length
+): Promise<U[]> {
+  return mapAllSettled(arr, mapFn, limit);
+}
+export default capParallel;
--- types.d.ts
+++
@@ -1,2 +0,0 @@
-type Mapper = <T,U>(value: T, index: number, array: readonly T[]) => U;
-declare async function capParallel<T,U>(arr:T[], mapFn:Mapper<T,U>, limit:number = arr.length):Promise<U[]>
