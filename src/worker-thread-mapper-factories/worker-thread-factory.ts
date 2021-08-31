068c9d98f7556ae97fb74ebb26019f6e5e9c671f
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-08-31 06:56:18 -0400

    incremental creation of the worker pool

---
+++ src/worker-thread-mapper-factories/worker-thread-factory.ts
@@ -0,0 +1,40 @@
+import * as worker_threads from 'worker_threads';
+import type { ThreadMapper } from '../types';
+import { workerFactory } from '../worker-thread-mapper-factories/worker-factory';
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// Copyright (c) 2021 Benjamin Vincent Kasapoglu (Luxcium)
+//
+
+// const threadWork: ThreadMapper<number, number> = (
+//   workerdata: WorkerData<number>
+// ): number => workerdata.value + 1;
+
+// WORKERFACTORY -------------------------------------------------------
+export const workerThreadFactory =
+  (filename: string) =>
+  <T, R>(threadWork: ThreadMapper<T, R>) =>
+    workerFactory(filename)(threadWork)(worker_threads);
+
+export const wThreadFactory =
+  <T, R>(threadWork: ThreadMapper<T, R>) =>
+  (filename: string) =>
+    workerFactory(filename)(threadWork)(worker_threads);
+
+// const [mainWorker_workPayload, threadWorker_startUnit] =
+//   workerFactory(__filename)(threadWork)(worker_threads);
+
+// THREADWORKER ----- ( implied:  NOT isMainThread ) -------------------
+// threadWorker_startUnit();
+
+// TEST WORK LOAD ------------------------------------------------------
+// const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
+//   mainWorker_workPayload<number>()<number>()
+// );
+// (async () =>
+//   worker_threads.isMainThread
+//     ? console.log(await Promise.all(values))
+//     : void 0)();
+//
+// Copyright (c) 2021 Benjamin Vincent Kasapoglu (Luxcium)
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
