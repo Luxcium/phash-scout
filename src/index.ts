6b5733ed5feda0ff62dad4d7f716a9faca8681c8
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-08-31 15:15:16 -0400

    remove the workerThreads argument/param

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
+import * as worker_threads from 'worker_threads';
 import parallelMapping from './parallel-mapping';
-import type { Mapper, WT_D } from './types';
+import type { Mapper } from './types';
 
 export const parallelMapperFactory =
   <T, R>(mappingFn: Mapper<T, R>) =>
   (list: T[], limit?: number) =>
-  (workerThreads: WT_D<T>) =>
   /**
    * The path to the Worker's main script or module.
    * @arg string filename
    * @see https://nodejs.org/dist/latest/docs/api/worker_threads.html#worker_threads_new_worker_filename_options
    */
   (filename: string) =>
-    parallelMapping<T, R>({ filename, workerThreads, list, mappingFn, limit });
+    parallelMapping<T, R>({
+      filename,
+      workerThreads: worker_threads,
+      list,
+      mappingFn,
+      limit,
+    });
 
 export default parallelMapping;
