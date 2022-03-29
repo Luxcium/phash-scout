252cebe41620057ba2652c97681ce9ff0d96f3ee
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-03-28 23:38:57 -0400

    fix: :bug: add the error type to the  FileType
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- src/core/types/Enums/FileTypeEnum.ts
+++ src/core/types/Enums/FileTypeEnum.ts
@@ -1,3 +1,5 @@
+import { WithFileType } from '../CurrentPath';
+
 export enum FileType {
   Directory = 'Directory',
   File = 'File',
@@ -7,4 +9,31 @@ export enum FileType {
   Socket = 'Socket',
   SymbolicLink = 'SymbolicLink',
   Unknown = 'Unknown',
+
+  Error = 'Error',
+}
+
+export function isDirectory(item: WithFileType) {
+  return item.type === FileType.Directory;
+}
+export function isFile(item: WithFileType) {
+  return item.type === FileType.File;
+}
+export function isBlockDevice(item: WithFileType) {
+  return item.type === FileType.BlockDevice;
+}
+export function isCharacterDevice(item: WithFileType) {
+  return item.type === FileType.CharacterDevice;
+}
+export function isFIFO(item: WithFileType) {
+  return item.type === FileType.FIFO;
+}
+export function isSocket(item: WithFileType) {
+  return item.type === FileType.Socket;
+}
+export function isSymbolicLink(item: WithFileType) {
+  return item.type === FileType.SymbolicLink;
+}
+export function isUnknown(item: WithFileType) {
+  return item.type === FileType.Unknown;
 }
