d01acb4532be7c64dcd71139337a7f2f71ce4d60
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-03-29 01:22:40 -0400

    npm run fix
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- src/packages/file-path/imports/types/index.ts
+++ src/packages/file-path/imports/types/index.ts
@@ -1,1 +1,9 @@
-../../../../core/classes/types/index.ts
+export type { CallbackfnT, CallbackfnU } from './CallbackfnT';
+export type { IApply } from './IApply';
+export type { IChain } from './IChain';
+export type { IMap } from './IMap';
+export type { IMapItems } from './IMapList';
+export type { IUnbox } from './IUnbox';
+export type { IUnboxList } from './IUnboxList';
+export type { Mapper } from './Mapper';
+export type { ThenMapper } from './ThenMapper';
--- src/private/sharp-phash/__sharp-phash-working.ts
+++ src/private/sharp-phash/__sharp-phash-working.ts
@@ -77,6 +77,7 @@ export async function main() {
         phash_,
         k,
       });
+
       getQueryResult = () => queryResult;
     }
 
