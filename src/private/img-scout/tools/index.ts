04f95fc04bdef098fc8cfc7ce02440b08d073332
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-05-18 14:24:33 -0400

    refactor: :recycle: move constants to index file
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- src/private/img-scout/tools/index.ts
+++ src/private/img-scout/tools/index.ts
@@ -1,3 +1,14 @@
 export { isA_Promise } from './isA_Promise';
 export { isQueryResult } from './isQueryResult';
 export { isQueryResultList } from './isQueryResultList';
+const QUERY = 'IMGSCOUT.QUERY';
+const ADD = 'IMGSCOUT.ADD';
+const SYNC = 'IMGSCOUT.SYNC';
+const DEL = 'IMGSCOUT.DEL';
+
+export const IMGSCOUT = {
+  QUERY,
+  ADD,
+  SYNC,
+  DEL,
+};
