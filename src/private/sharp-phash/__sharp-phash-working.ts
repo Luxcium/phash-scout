eaeef7fa842066c59ed803b1900dbf5af3bca987
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-05-28 18:41:09 -0400

    feat: :construction: add support for display prompt in CLI
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- src/private/sharp-phash/__sharp-phash-working.ts
+++ src/private/sharp-phash/__sharp-phash-working.ts
@@ -14,6 +14,75 @@ import { rConnect } from './rConnect';
 
 const humanSize = require('human-size');
 
+export const CSI = (Pt: string) => `\u009B${Pt}`;
+
+export const CUD = (Ps: number) => CSI(`${Ps}B`);
+export const CUU = (Ps: number) => CSI(`${Ps}A`);
+export const SM = (Ps: number) => CSI(`${Ps}h`);
+/** **Erase In Display:** Erase various parts of the viewport. */
+export const ED = (Ps: number) => CSI(`${Ps}J`);
+/**  **Erase In Display:** `0` Erase from the cursor through the end of the viewport. */
+export const ED0 = ED(0);
+/**  **Erase In Display:** `1` Erase from the beginning of the viewport through the cursor. */
+export const ED1 = ED(1);
+/**  **Erase In Display:** `2` Erase complete viewport. */
+export const ED2 = ED(2);
+/**  **Erase In Display:** `3` Erase scrollback. */
+export const ED3 = ED(3);
+
+/** 2	Keyboard Action Mode (KAM). Always on.	✗ */
+export const SM2 = SM(2);
+/** 4	Insert Mode (IRM).	✓ */
+export const SM4 = SM(4);
+/** 12	Send/receive (SRM). Always off.	✗ */
+export const SM12 = SM(12);
+/** 20	Automatic Newline (LNM). Always off.	✗ */
+export const SM20 = SM(20);
+export const RM = (Ps: number) => CSI(`${Ps}l`);
+/**2	Keyboard Action Mode (KAM). Always on.	✗   */
+export const RM2 = RM(2);
+/** 4	Replace Mode (IRM). (default)	✓   */
+export const RM4 = RM(4);
+/** 12	Send/receive (SRM). Always off.	✗   */
+export const RM12 = RM(12);
+/** 20	Normal Linefeed (LNM). Always off.	✗   */
+export const RM20 = RM(20);
+export const DECSET = (Ps: string) => CSI(`?${Ps}h`);
+export const DECRST = (Ps: string) => CSI(`?${Ps}l`);
+
+process.title = 'Sharp pHash';
+//+ cliCursor hide
+console.error('cliCursor hide [?25l]');
+process.stderr.write('\u009B?25l');
+//+ Use Alternate Screen Buffer.
+console.error(
+  'Save cursor and switch to alternate buffer clearing it. [?1049h]'
+);
+console.error('\u009B?1049h');
+
+process.on('SIGINT', () => {
+  process.exitCode = 2; //+ 128;
+  throw new Error();
+});
+
+process.on('SIGTERM', () => {
+  process.exitCode = 15; //+ 128;
+  throw new Error();
+});
+process.on('beforeExit', code => {
+  console.error('\u009B!p');
+  console.error('\u001Bc');
+  //+ Use Normal Screen Buffer (clearing screen if in alt).
+  console.error('Use Normal Screen Buffer [?1049l]');
+  console.error('\u009B?1049l');
+  console.error(code - 128);
+  console.error('Use Normal Screen Buffer and restore cursor. [?1049l]');
+  //+ cliCursor show
+  console.error('cliCursor show [?25h]');
+  console.error('\u009B?25h');
+  console.error(`About to exit with code: ${code - 128}`);
+});
+
 export const count = { index1: 1 };
 
 export async function main() {
@@ -85,7 +154,8 @@ export async function main() {
               qrItem =>
                 (qrItem as QueryResultObject).radius === '0' &&
                 count2.a1++ === 0 &&
-                console.log(result)
+                // console.error();
+                console.log(ED2, CUD(10_000), result.queryResult)
             );
             // if (result.queryResult) {
             // console.log(result);
@@ -108,4 +178,5 @@ export async function main() {
 
   return result;
 }
+
 main();
