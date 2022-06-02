6d79ef20a9ca2aa926c79f81d61d979ee94b7d5f
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-06-01 23:26:47 -0400

    refactor: :truck: rename file
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

---
+++ src/private/sharp-phash/sharp-phash-working.ts
@@ -0,0 +1,154 @@
+import { statSync } from 'fs';
+import { isArray } from '../../core/utils';
+import {
+  notExcluded,
+  notNull,
+} from '../../packages/file-path/tools/notExclude';
+import type {
+  Bg,
+  WithBaseName,
+  WithDir,
+  WithExtname,
+  WithFullPath,
+  WithPHash,
+} from '../../packages/file-path/types';
+import { WithExclude } from '../../packages/file-path/types';
+import { immediateZalgo } from '../../utilities/utils';
+import { QueryResultItem, QueryResultObject } from '../img-scout/types';
+import { uniqueAddToObj } from '../img-scout/uniqueAddToObj';
+import { Strange } from './types';
+
+const humanSize = require('human-size');
+// ${
+// stats.size > 0 ? humanSize(stats.size, 2) || 0 : 0
+// }:
+export const count = { index1: 1 };
+
+export function doRedisQuery(
+  R: any,
+
+  key: string
+) {
+  return (
+    bgPaths: Bg<
+      Promise<
+        WithDir &
+          WithFullPath &
+          WithBaseName &
+          WithExtname &
+          WithExclude &
+          WithPHash
+      >
+    >
+  ): Bg<Promise<Strange>> => {
+    return bgPaths.map(async paths => {
+      const awtPaths = await paths;
+      let getQueryResult = (): any => null;
+      let queryResult: Array<QueryResultItem | QueryResultObject> | null = null;
+
+      if (notNull(awtPaths.pHash) && notExcluded(awtPaths)) {
+        const { fullPath, dir, extname, baseName } = awtPaths;
+        const stats = statSync(fullPath);
+        const phash_ = awtPaths.pHash;
+        const title = `${key}:${
+          stats.size > 0 ? humanSize(stats.size, 2) || 0 : 0
+        }:${stats.size}:${dir}:${baseName}:${extname
+          .split('.')
+          .slice(1)
+          .join('')}`;
+        queryResult = await uniqueAddToObj({
+          R,
+          title,
+          phash_,
+          k: key,
+        });
+        getQueryResult = () => queryResult;
+      }
+      const strange: Strange = {
+        queryResult,
+        ...(await paths),
+        getQueryResult,
+      };
+      return immediateZalgo(strange);
+    });
+  };
+}
+
+export function manageRedisQuery(bgItem: Bg<Promise<Strange>>) {
+  return bgItem.map(async item => {
+    const waited = await item;
+    try {
+      if (notExcluded(waited)) {
+        const { queryResult, ...awaited } = waited;
+        if (queryResult) {
+          const result = {
+            queryResult, //: queryResult.reverse(),
+            ...awaited,
+          };
+          const count2 = { a1: 0 };
+          // HACK:
+          // console.log(
+          //   // ED2,
+          //   // CUD(10_000),
+          //   result.queryResult,
+          //   result.queryResult.length,
+          //   count2.a1++
+          // );
+          result.queryResult.map(
+            qrItem =>
+              (qrItem as QueryResultObject).radius === '0' &&
+              count2.a1++ === 0 &&
+              console.log(/* ED2, CUD(10_000), */ result.queryResult.reverse())
+          );
+
+          return result.queryResult;
+        }
+      }
+    } catch (error) {
+      console.error('in boxedGenerator3', error);
+    }
+    return item;
+  });
+}
+
+export function describeMapping<T>(fn: <R>(val: T) => R) {
+  return (mapable: T[] | Bg<T>) => {
+    if (isArray(mapable)) {
+      return mapable.map(fn);
+    }
+    return mapable.map(fn);
+  };
+}
+
+export function composer<T, A, R>(fn1: (val: A) => R, fn2: (val: T) => A) {
+  return (x: T) => fn1(fn2(x));
+}
+export function piper<T, A, R>(fn1: (val: T) => A, fn2: (val: A) => R) {
+  return (x: T) => fn2(fn1(x));
+}
+
+export const le2n: (x: string) => number = composer(
+  (n: number) => n * 2,
+  (t: string) => t.length
+);
+export const le4n: (x: string) => number = composer((n: number) => n * 2, le2n);
+
+export const len2: (x: string) => number = piper(
+  (t: string) => t.length,
+  (n: number) => n * 2
+);
+export const len4: (x: string) => number = piper(len2, (n: number) => n * 2);
+
+export function listComposer<T, A, R>(fn1: (val: A) => R, fn2: (val: T) => A) {
+  return (x: T[]) => x.map(fn2).map(fn1);
+}
+export function listPiper<T, A, R>(fn1: (val: T) => A, fn2: (val: A) => R) {
+  return (x: T[]) => x.map(fn1).map(fn2);
+}
+
+export function bgPiper<T, A, R>(fn1: (val: T) => A, fn2: (val: A) => R) {
+  return (x: Bg<T>) => x.map(fn1).map(fn2);
+}
+export function bgComposer<T, A, R>(fn1: (val: A) => R, fn2: (val: T) => A) {
+  return (x: Bg<T>) => x.map(fn2).map(fn1);
+}
