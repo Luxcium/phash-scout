f7fecc53618843f690d4d5abe49ae73e0bd6fb61
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-06-26 08:52:11 -0400

    refactor: :alien: remove cross dependencies to core frm private
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

---
+++ src/private/types/TX.ts
@@ -0,0 +1,30 @@
+import type { CurrentPath } from '../file-path/types';
+import type { PQuerryAndAdd, QueryResultItem } from '../img-scout/types';
+// /home/luxcium/projects/parallel-mapping/src/utilities/files/file-path/types/index.ts
+// /home/luxcium/projects/parallel-mapping/src/../../utilities/files/file-path/types
+import { FALSY } from '../../core/types/Falsy';
+import { N, S } from '../../core/types/IQueryListPhash';
+
+export type TX = Promise<{
+  transact: Promise<QueryResultItem[]>;
+  path: CurrentPath;
+  pHash: {
+    willPhash_?: () => Promise<S | null>;
+    index: N;
+    value: null | string;
+  };
+  pQuerryAndAdd?: PQuerryAndAdd;
+}>;
+
+export type Tx = Promise<{
+  transact: FALSY | Promise<QueryResultItem[]>;
+  path: FALSY | CurrentPath;
+  pHash:
+    | FALSY
+    | {
+        willPhash_?: () => Promise<S | null>;
+        index: N;
+        value: null | string;
+      };
+  pQuerryAndAdd?: FALSY | PQuerryAndAdd;
+}>;
---
+++ src/private/utils/paths/getCurrentPath.ts
@@ -0,0 +1,75 @@
+import path from 'path';
+import type { DirentWithFileType } from '../../../core/types';
+import { FileType } from '../../../core/types/Enums/FileTypeEnum';
+import {
+  BlockDevicePath,
+  CharacterDevicePath,
+  CurrentPath,
+  DirectoryPath,
+  FIFOPath,
+  FilePath,
+  SocketPath,
+  SymbolicLinkPath,
+  UnknownTypePath,
+} from '../../file-path/types';
+
+/** @deprecated */
+export const currentPath = (folder: string) => (f: DirentWithFileType) =>
+  getCurrentPath(f, folder);
+
+/*
+    dir: folderPath,
+    fullPath: `${folderPath}/${f.fileName}`,
+    fileName: f.fileName,
+    type: FileType.Unknown,
+ */
+/** @deprecated */
+export function getCurrentPath(f: DirentWithFileType, folderPath: string) {
+  const extname = path.extname(`${f.fileName}`);
+  const ext = extname.toLowerCase();
+  const fullPath = `${folderPath}/${f.fileName}`;
+  const parsed = path.parse(fullPath);
+  const fullPath_: CurrentPath = {
+    dir: folderPath,
+    extname,
+    ext,
+    fullPath,
+    fileName: f.fileName,
+    baseName: parsed.name,
+    type: FileType.Unknown,
+    exclude: false,
+  };
+
+  if (f.isDirectory) {
+    fullPath_.type = FileType.Directory;
+    return fullPath_ as DirectoryPath;
+  }
+  if (f.isFile) {
+    fullPath_.type = FileType.File;
+    return fullPath_ as FilePath;
+  }
+  if (f.isSymbolicLink) {
+    fullPath_.type = FileType.SymbolicLink;
+    return fullPath_ as SymbolicLinkPath;
+  }
+  if (f.isBlockDevice) {
+    fullPath_.type = FileType.BlockDevice;
+    return fullPath_ as BlockDevicePath;
+  }
+  if (f.isCharacterDevice) {
+    fullPath_.type = FileType.CharacterDevice;
+    return fullPath_ as CharacterDevicePath;
+  }
+
+  if (f.isFIFO) {
+    fullPath_.type = FileType.FIFO;
+    return fullPath_ as FIFOPath;
+  }
+
+  if (f.isSocket) {
+    fullPath_.type = FileType.Socket;
+    return fullPath_ as SocketPath;
+  }
+
+  return fullPath_ as UnknownTypePath;
+}
