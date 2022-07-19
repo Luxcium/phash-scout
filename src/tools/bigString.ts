dff52ddca40c2a9e3fbe2ee5513152d4077ce82c
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-07-19 16:43:03 -0400

    fix: :memo: Change error messages in bigString / pHash calculations.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- src/tools/bigString.ts
+++ src/tools/bigString.ts
@@ -1,25 +1,5 @@
 import { S } from '../types';
 
-// export function bigString_(str: S): S;
-// export function bigString_(str: Promise<S>): Promise<S>;
-// export function bigString_(str: S | Promise<S>): S | Promise<S> {
-//   if (typeof str === 'object' && str instanceof Promise) return immediate(str);
-
-//   return bigString(str);
-// }
-
-// async function immediate(str: Promise<S>): Promise<S> {
-//   try {
-//     return immediateZalgo(bigString(await str));
-//   } catch (error) {
-//     console.error(
-//       '>     \u009B31mFailed Silently in: immediate ~~>bigString_(await str)\u0007 \u009B0m',
-//       `${error}`
-//     );
-//   }
-//   return '';
-// }
-
 export function bigString(str: S): S {
   const strSplit = [...str];
 
--- src/tools/computePHash/getBigStrPHash.ts
+++ src/tools/computePHash/getBigStrPHash.ts
@@ -8,25 +8,29 @@ export async function getBigStrPHashFromFile(
   imgFilePath: string
 ): Promise<string> {
   try {
-    const thisImage = readFileImgFile(imgFilePath);
-    const sharpPhashValue = calculateSharpPhash(await thisImage);
+    const thisImage = await fs.promises.readFile(imgFilePath);
+    const sharpPhashValue = calculateSharpPhash(thisImage);
     const returnValue = calculateBigString(await sharpPhashValue);
+
     return await immediateZalgo(returnValue);
   } catch (error) {
     console.error(
-      '> \u009B90m at getBigStrPHashFromFile\u009B0m',
-      `\u009B31m>\u009B0m \u009B37m\u009B01m${error}\u009B0m`.replace(/\n/g, '')
+      '',
+      '    \u009B90m> at getBigStrPHashFromFile\u009B0m\n\u009B90m',
+      `    \u009B91m> \u009B01m\u009B4m${error}\u009B0m`.replace(/\n/g, ''),
+      `\n\n     > '${imgFilePath}' !!!\u009B0m\n\u009B0m`
     );
+    return '0n';
   }
-  return '';
 }
 
-async function readFileImgFile(imgFilePath: string): Promise<Buffer> {
+export async function readFileImgFile(imgFilePath: string): Promise<Buffer> {
   try {
     return await fs.promises.readFile(imgFilePath);
   } catch (error) {
     console.error(
-      '\n> \u009B90m at readFileImgFile in getBigStrPHashFromFile can not readFile from:\u0007 \u009B0m'
+      '\n',
+      '    \u009B90m> at readFileImgFile in getBigStrPHashFromFile can not readFile from:\u0007 \u009B0m'
     );
     throw error;
   }
@@ -37,7 +41,8 @@ async function calculateSharpPhash(thisImage: Buffer): Promise<string> {
     return await sharpPhash(thisImage);
   } catch (error) {
     console.error(
-      '\n> \u009B90m at calculateSharpPhash in getBigStrPHashFromFile can not process image file,\u0007 \u009B0m'
+      '\n',
+      '    \u009B90m> at calculateSharpPhash in getBigStrPHashFromFile can not process image file,\u0007 \u009B0m'
     );
     throw error;
   }
@@ -48,7 +53,8 @@ function calculateBigString(sharpPhashValue: string): string {
     return bigString(sharpPhashValue);
   } catch (error) {
     console.error(
-      '\n> \u009B90m at calculateBigString in getBigStrPHashFromFile can not calcualate bigString,\u0007 \u009B0m'
+      '\n',
+      '    \u009B90m> at calculateBigString in getBigStrPHashFromFile can not calcualate bigString,\u0007 \u009B0m'
     );
     throw error;
   }
