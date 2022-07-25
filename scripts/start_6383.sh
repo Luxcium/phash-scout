5f43d623b9aefa3966f49e185b73dc79e0237734
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-07-25 06:44:12 -0400

    feat: :hammer: Update script and tasks to start the container and monitor redis.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- scripts/start_6383.sh
+++ scripts/start_6383.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-# source "${CORE_FUNCTIONS}/04000-shelltype.sh"
-/usr/src/Redis-ImageScout/docker_run_redis
-# isloginorinteractive ||  exit
 
-# Boost v1.67
-# libpng-dev
-# libtiff-dev
-# libjpeg-dev
+REDIS_6383_PING=$(redis-cli -p 6383 PING)
+if [ "$REDIS_6383_PING" != 'PONG' ]; then
+    /usr/src/Redis-ImageScout/docker_run_redis
+fi
+echo 'already runing an instance of redis-cli <port> 6383'
+sleep 2
+echo 'Will now monitor:'
+redis-cli -p 6383 monitor
