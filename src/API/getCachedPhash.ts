7bf8ec8ad5394d5204c5196960449f38f262986f
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-07-25 06:41:31 -0400

    refactor: :truck: Rename function  and file.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

---
+++ src/API/getCachedPhash.ts
@@ -0,0 +1,21 @@
+import { immediateZalgo } from '../utils';
+import { SET } from './_TraverseDirs2';
+
+// HACK:  caching pHash in redis -------------------------------------
+
+export async function getCachedPhash(
+  RC: any,
+  k_FullPath: string,
+  getValueFnct: (fullPath: string) => Promise<string>
+) {
+  const K = `'cachedPhash:${k_FullPath}'`;
+  const R = await RC;
+
+  let value: Promise<string> | string = (await R.GET(K)) as string;
+  if (value !== null && value.toString().length < 10) {
+    return immediateZalgo(value);
+  }
+  value = getValueFnct(k_FullPath);
+  SET(R, K, value);
+  return immediateZalgo(value);
+}
