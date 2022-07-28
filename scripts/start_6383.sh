4bbc2087d76e9e8d678871914b13ab26681d5ec6
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-07-28 15:24:10 -0400

    chore: :hammer: Update redis cli start up method to display the port.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- scripts/start_6383.sh
+++ scripts/start_6383.sh
@@ -4,7 +4,7 @@ REDIS_6383_PING=$(redis-cli -p 6383 PING)
 if [ "$REDIS_6383_PING" != 'PONG' ]; then
     /usr/src/Redis-ImageScout/docker_run_redis
 fi
-echo 'already runing an instance of redis-cli <port> 6383'
+echo 'already runing an instance of redis-cli <port=6383>'
 sleep 2
 echo 'Will now monitor:'
 redis-cli -p 6383 monitor
