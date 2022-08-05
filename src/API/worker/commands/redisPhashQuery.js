2db1bd921d6d9da7c9d5c457595539bfeebbda46
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-08-05 15:35:05 -0400

    fix: :fire: Remove unused variable.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- src/API/worker/commands/redisPhashQuery.js
+++ src/API/worker/commands/redisPhashQuery.js
@@ -1,11 +1,9 @@
 'use strict';
-import { commands, Rc } from '.';
+import { commands } from '.';
 import { getRedisQueryResult } from './getRedisQueryResult';
 import { pathPaser } from './pathPaser';
 
 export async function redisPhashQuery(imgFileAbsPath) {
-  const RC = await Rc;
-
   const { pathInfos } = pathPaser(imgFileAbsPath);
   if (pathInfos.extname !== '.jpg') {
     return ['not .jpg'];
