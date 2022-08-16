0e00555383b760131019f0d81c49c06f4cf74140
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-08-16 14:39:56 -0400

    feat: :zap: Add pipelining to the redis.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- src/commands/delPhash.ts
+++ src/commands/delPhash.ts
@@ -18,6 +18,23 @@ export async function delPhash(R: any, k: S, id: N, failSilently = true) {
   }
   return [];
 }
+export async function delPhash2(R: any, k: S, id: N, failSilently = true) {
+  try {
+    const R_EXISTS = await R.EXISTS(k);
+    if (R_EXISTS === 1) {
+      const commands = [syncPhash(R, k), R.sendCommand([IMGSCOUT.DEL, k, id])];
+      const results = await Promise.all(commands);
+      await syncPhash(R, k);
+      // const result = R.sendCommand([IMGSCOUT.DEL, k, id]);
+      return results[1];
+    }
+    console.error(`R.EXISTS(${k}) -> ${R_EXISTS}`);
+  } catch (error: any) {
+    if (!failSilently) throw new Error('queryPhash' + error);
+    console.error('\u009B31mqueryPhash Failled silently\u009B0m');
+  }
+  return [];
+}
 
 export const delPhashKey =
   (R: any, k: S, failSilently = true) =>
