2351b2aa21c45a2f4373d8adcf5c041bda3e63ce
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-10-13 04:43:17 -0400

    BACKUP 2022 OCTOBER 13

--- src/API/directory-traversal/ScanDirs/main.ts
+++ src/API/directory-traversal/ScanDirs/main.ts
@@ -1,39 +1,67 @@
-import { BoxedAsyncGenerator } from '@luxcium/boxed-list';
+import * as dotenv from 'dotenv';
+import { statSync } from 'fs';
+import humanSize from 'human-size';
 
-import { getCachedPhash } from '../tools/getCachedPhash';
 import { ScanDirs } from './ScanDirs';
 
+dotenv.config();
 export async function main() {
-  const scaner = ScanDirs.from(
-    '/media/luxcium/Archive_Locale/import/GAYBOYSTUBE/users'
-  ).addValidExt(['jpg']);
+  const fullRootPath = process.env['scan_2TB_dir'] || '';
+  console.log('# scan dir:', fullRootPath);
 
-  void ((filePath: string) => {
-    // console.log(filePath);
-    return filePath;
-  });
-
-  const asyncGen = BoxedAsyncGenerator.fromGen<any>(() => scaner.map())
-    .mapAwait((filePath: string) => {
-      //  console.log(filePath);
-      return filePath;
-    })
-    .mapAwait(filepath => getCachedPhash(filepath));
-  for await (const path of asyncGen.asyncGen()) {
-    console.log(path);
+  const scaner = ScanDirs.from(fullRootPath)
+    .addValidExt(['.mp4', '.mov'])
+    .map((filePath: string) => {
+      return [
+        statSync(filePath).size,
+        `${filePath.split(`${fullRootPath}/`)[1]}`,
+      ];
+    });
+  console.log(`fullRootPath="${fullRootPath}"`);
+  let count = 1;
+  for await (const path of scaner) {
+    // console.log((path as any)[1]);
+    if (Number((path as any)[0]) > 1024 * 1024 * 1) {
+      console.log(
+        `ln "\${fullRootPath}/${(path as any)[1]}" "${count++}-${(
+          path as string[]
+        )[1]
+          .split('/')
+          .slice(-1)}" # ${humanSize((path as any)[0])}`
+      );
+    }
   }
-  asyncGen;
-  //   .map(filepath => getCachedPhash(filepath))
-  //   .map(async phash => console.log(await phash))
-  //   .asyncSpark();;
-  // BoxedGenerator.fromGen(() => scaner)
-  //   .map((filePath: string) => {
-  //     // console.log(filePath);
-  //     return filePath;
-  //   })
-  //   .map(filepath => getCachedPhash(filepath))
-  //   .map(async phash => console.log(await phash))
-  //   .asyncSpark();
 }
 
 main();
+
+// for (const iterator of scaner) {
+// }
+
+// void ((filePath: string) => {
+//   // console.log(filePath);
+//   return filePath;
+// });
+
+// const asyncGen = BoxedAsyncGenerator.fromGen<any>(() => scaner.map())
+//   .mapAwait((filePath: string) => {
+//     console.log(filePath);
+//     return filePath;
+//   })
+//   .mapAwait(filepath => getCachedPhash(filepath));
+
+// BoxedAsyncGenerator;
+// getCachedPhash;
+// scaner;
+// asyncGen;
+//   .map(filepath => getCachedPhash(filepath))
+//   .map(async phash => console.log(await phash))
+//   .asyncSpark();;
+// BoxedGenerator.fromGen(() => scaner)
+//   .map((filePath: string) => {
+//     // console.log(filePath);
+//     return filePath;
+//   })
+//   .map(filepath => getCachedPhash(filepath))
+//   .map(async phash => console.log(await phash))
+//   .asyncSpark();
--- types/human-size.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'human-size';
-
-type Mags = ' ' | 'K' | 'M' | 'G' | 'T' | 'P' | 'E' | 'Z' | 'Y';
-export default function humanSize(
-  bytes: number,
-  precision?: number
-): `${number}B${Mags}`;
