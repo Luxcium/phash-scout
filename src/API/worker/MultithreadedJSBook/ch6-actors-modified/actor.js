81d2be831add26a0d646a2ebf6f247020cb2805e
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-11-10 06:09:29 -0500

    Add models from book Multithreaded JavaScript:
    MIT LICENSE
    Copyright © 2021-2022 Benjamin Vincent Kasapoglu (Luxcium)
    NOTICE:
    O’Reilly Online Learning
    Title: “Multithreaded JavaScript”
    Author: “by Thomas Hunter II and Bryan English”
    Publisher: “O’Reilly”
    Copyright: “© 2022 Thomas Hunter II and Bryan English”
    ISBN: “978-1-098-10443-6.”
    Using Code Examples
    Supplemental material (code examples, exercises, etc.)
    is available for download at
    https://github.com/MultithreadedJSBook/code-samples.
    In general, if example code is offered with this book, you may
    use it in your programs and documentation. You do not need to
    contact us for permission unless you’re reproducing a
    significant portion of the code. For example, writing a
    program that uses several chunks of code from this book does
    not require permission. Selling or distributing examples from
    O’Reilly books does require permission. Answering a question by
    citing this book and quoting example code does not require
    permission. Incorporating a significant amount of example code
    from this book into your product’s documentation does require
    permission.
    If you feel your use of code examples falls outside fair use or
    the permission given above, feel free to contact us at
    permissions@oreilly.com
    
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

---
+++ src/API/worker/MultithreadedJSBook/ch6-actors-modified/actor.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+const net = require('net');
+const RpcWorkerPool = require('./rpc-worker.js');
+
+const [, , host] = process.argv;
+const [hostname, port] = host.split(':');
+// XXX:
+const worker = new RpcWorkerPool(
+  '/home/luxcium/projects/phash-scout/src/API/worker/MultithreadedJSBook/ch6-actors/worker.js',
+  4,
+  'roundrobin'
+);
+// THIS TEXT SHOULD NOT APPEAR IN BOOK
+const upstream = net
+  .connect(port, hostname, () => {
+    console.log('connected to server');
+  })
+  .on('data', async raw_data => {
+    const chunks = String(raw_data).split('\0'); // <1>
+    chunks.pop();
+    for (let chunk of chunks) {
+      const data = JSON.parse(chunk);
+      const value = await worker.exec(data.method, ...data.args);
+      upstream.write(
+        JSON.stringify({
+          id: data.id,
+          value,
+          pid: process.pid,
+        }) + '\0'
+      );
+    }
+  })
+  .on('end', () => {
+    console.log('disconnect from server');
+  });
+/* **************************************************************** */
+/*                                                                  */
+/*  MIT LICENSE                                                     */
+/*                                                                  */
+/*  Copyright © 2021-2022 Benjamin Vincent Kasapoglu (Luxcium)      */
+/*                                                                  */
+/*  NOTICE:                                                         */
+/*  O’Reilly Online Learning                                        */
+/*                                                                  */
+/*  Title: “Multithreaded JavaScript”                               */
+/*  Author: “by Thomas Hunter II and Bryan English”                 */
+/*  Publisher: “O’Reilly”                                           */
+/*  Copyright: “© 2022 Thomas Hunter II and Bryan English”          */
+/*  ISBN: “978-1-098-10443-6.”                                      */
+/*                                                                  */
+/*  Using Code Examples                                             */
+/*  Supplemental material (code examples, exercises, etc.)          */
+/*  is available for download at                                    */
+/*  https://github.com/MultithreadedJSBook/code-samples.            */
+/*                                                                  */
+/*  In general, if example code is offered with this book, you may  */
+/*  use it in your programs and documentation. You do not need to   */
+/*  contact us for permission unless you’re reproducing a           */
+/*  significant portion of the code. For example, writing a         */
+/*  program that uses several chunks of code from this book does    */
+/*  not require permission. Selling or distributing examples from   */
+/*  O’Reilly books does require permission. Answering a question by */
+/*  citing this book and quoting example code does not require      */
+/*  permission. Incorporating a significant amount of example code  */
+/*  from this book into your product’s documentation does require   */
+/*  permission.                                                     */
+/*                                                                  */
+/*  If you feel your use of code examples falls outside fair use or */
+/*  the permission given above, feel free to contact us at          */
+/*  permissions@oreilly.com.                                        */
+/*                                                                  */
+/* **************************************************************** */
